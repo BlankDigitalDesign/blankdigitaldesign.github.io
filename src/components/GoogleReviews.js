b36ba616e0388eb10125f1ca686c672df208a52c
Joshua Blank <jmnblnk@yahoo.com>
2023-07-28 16:03:39 -0400

    update font

--- src/components/GoogleReviews.js
+++ src/components/GoogleReviews.js
@@ -2,6 +2,7 @@ import React from "react";
 import { ElfsightWidget } from "react-elfsight-widget";
 
 const GoogleReviews = () => {
+  console.log(ElfsightWidget);
   return <ElfsightWidget widgetID="bb4c5d6f-abb9-4c0e-b6a1-2299ade1df5d" />;
 };
 // {process.env.REACT_APP_ELFSIGHT_ID}
